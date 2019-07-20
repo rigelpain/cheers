3a1fd4e46b1ce71ef0ff3743d40cd93ecbd503be
rigelpain <rigelpain@gmail.com>
2019-07-21 00:13:58 +0900

    all:全ボタンに色変更適応

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
 
+window.onload =function(){
+    document.getElementById('n1').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n1').style.background = '#FFC9F4';
+    }
 
-function BackgroundChangeColor1() {
-    NumberBackgroundClean();
-    document.getElementById('n1').style.background = '#FFC9F4';
-}
+    document.getElementById('n2').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n2').style.background = '#ACEEFF';
+    }
 
-function BackgroundChangeColor2() {
-    NumberBackgroundClean();
-    document.getElementById('n2').style.background = '#ACEEFF';
-}
+    document.getElementById('n3').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n3').style.background = '#80ff7dd6';
+    }
 
-function agree(){
+    document.getElementById('n4').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n4').style.background = '#80ff7dd6';
+    }
 
-}
+    document.getElementById('n5').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n5').style.background = '#80ff7dd6';
+    }
 
-function NumberBackgroundClean() {
-    var elements = document.getElementsByClassName('number');
-    for(i=0; i<elements.length; i++){
-        elements[i].style.backgroundColor = '#FFF';
+    document.getElementById('n6').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n6').style.background = '#80ff7dd6';
+    }
+
+    document.getElementById('n7').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n7').style.background = '#80ff7dd6';
+    }
+
+    document.getElementById('n8').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n8').style.background = '#80ff7dd6';
+    }
+
+    document.getElementById('n9').onclick = function(){
+      NumberBackgroundClean();
+      document.getElementById('n9').style.background = '#80ff7dd6';
+    }
+
+    function NumberBackgroundClean() {
+        var elements = document.getElementsByClassName('number');
+        for(i=0; i<elements.length; i++){
+            elements[i].style.backgroundColor = '#FFF';
+        }
+    }
+
+    document.getElementById('agree').onclick = function(){
+      document.getElementById('agree').style.background = '#AAA';
     }
 }
 
