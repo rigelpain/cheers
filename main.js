e783fa6fc4fcf5cd4260fe1b03aec5c4c42dd1ca
rigelpain <rigelpain@gmail.com>
2019-07-22 18:45:21 +0900

    html:変化なし,css:dialnumbersにactiveクラスverを追加,js:クリックで色付けから色抜きを単純なbackground変更からactiveクラス<->numberクラスへの変更を使って一挙動化>>DialClick()

--- main.js
+++ main.js
@@ -1,67 +1,59 @@
+window.onload = function() {
 
-window.onload =function(){
-    document.getElementById('n1').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n1').style.background = '#FFC9F4';
+    document.getElementById('n1').onclick = function() {
+        DialClick("n1");
     }
 
-    document.getElementById('n2').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n2').style.background = '#ACEEFF';
+    document.getElementById('n2').onclick = function() {
+        DialClick("n2");
     }
 
-    document.getElementById('n3').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n3').style.background = '#80ff7dd6';
+    document.getElementById('n3').onclick = function() {
+        DialClick("n3");
     }
 
-    document.getElementById('n4').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n4').style.background = '#80ff7dd6';
+    document.getElementById('n4').onclick = function() {
+        DialClick("n4");
     }
 
-    document.getElementById('n5').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n5').style.background = '#80ff7dd6';
+    document.getElementById('n5').onclick = function() {
+        DialClick("n5");
     }
 
-    document.getElementById('n6').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n6').style.background = '#80ff7dd6';
+    document.getElementById('n6').onclick = function() {
+        DialClick("n6");
     }
 
-    document.getElementById('n7').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n7').style.background = '#80ff7dd6';
+    document.getElementById('n7').onclick = function() {
+        DialClick("n7");
     }
 
-    document.getElementById('n8').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n8').style.background = '#80ff7dd6';
+    document.getElementById('n8').onclick = function() {
+        DialClick("n8");
     }
 
-    document.getElementById('n9').onclick = function(){
-      NumberBackgroundClean();
-      document.getElementById('n9').style.background = '#80ff7dd6';
+    document.getElementById('n9').onclick = function() {
+        DialClick("n9");
+    }
+
+    document.getElementById('agree').onclick = function() {
+        target = document.getElementById('agree');
+        target.style.background = '#CCC';
+        setTimeout("target.style.background = '#FFF';", 500);
+    }
+
+    function DialClick(id) {
+        target = document.getElementById(id);
+        target.className = "active";
+        setTimeout("target.className = 'number'", 200);
     }
 
     function NumberBackgroundClean() {
         var elements = document.getElementsByClassName('number');
-        for(i=0; i<elements.length; i++){
+        for (i = 0; i < elements.length; i++) {
             elements[i].style.backgroundColor = '#FFF';
         }
     }
 
-    document.getElementById('agree').onclick = function(){
-      document.getElementById('agree').style.background = '#AAA';
-    }
-}
 
-function clickTest(){
-    target = document.getElementById("n8");
-    if(target.className == null || target.className == "number") {
-      target.className = "active";
-    }else {
-      target.className = "number";
-    }
 }
